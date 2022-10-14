2206b6b30a812b0d2f840b3d03f3c77489ad065b
TGDarkLord <113157573+TGDarkLord@users.noreply.github.com>
2022-10-14 11:10:52 +0530

    Update utils.py

--- plugins/utils/utils.py
+++ plugins/utils/utils.py
@@ -18,8 +18,8 @@ async def progress_message(current, total, ud_type, message, start):
         estimated_total_time = TimeFormatter(milliseconds=estimated_total_time)
 
         progress = "[{0}{1}]".format(
-            ''.join(["■" for i in range(math.floor(percentage / 5))]),
-            ''.join(["□" for i in range(20 - math.floor(percentage / 5))]),
+            ''.join(["●" for i in range(math.floor(percentage / 5))]),
+            ''.join(["◌" for i in range(20 - math.floor(percentage / 5))]),
             )
 
         tmp = progress + Translation.PROGRESS_BAR.format(
