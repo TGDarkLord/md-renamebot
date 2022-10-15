da8df130c9880cce979ecbf66d075afca50620e0
TGDarkLord <113157573+TGDarkLord@users.noreply.github.com>
2022-10-15 09:37:22 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -75,9 +75,10 @@ async def cb_handler(client: Client , query: CallbackQuery):
              ],[
              InlineKeyboardButton("📢 Project Channel", url="https://t.me/TechProjects_2022"),
              InlineKeyboardButton("♻️ Suppot Group", url="https://t.me/+uBASAptv8aJkMDU1")
-           )]]
+           ]]
 
-
+       )
+   
    )
 
     elif data == "help":
